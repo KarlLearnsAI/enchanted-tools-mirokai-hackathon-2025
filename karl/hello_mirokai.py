e1456d86fa9177c8dad21ab702df3f13eec89552
Karl Johannes <karl.frjo@gmail.com>
2025-06-07 14:43:40 +0100

    museum guide v1.1

--- karl/hello_mirokai.py
+++ karl/hello_mirokai.py
@@ -2,13 +2,15 @@ import asyncio
 from pymirokai.robot import connect
 from pymirokai.models.data_models import Coordinates
 
-robot_ip = "10.32.38.150"
+robot_ip = "10.6.32.15"
 api_key  = "admin"
 
 async def main():  
     async with connect(api_key, robot_ip) as robot:
-        mission = robot.go_to_relative(Coordinates(x=1.0, y=2.0, theta=0.0))
-        mission = robot.say("Hello, world!")
+        walk = robot.go_to_relative(Coordinates(x=1.0, y=2.0, theta=0.0))
+        await walk.completed()
+        saystatement = robot.say("Hello, museum enthusiasts!")
+        await saystatement.completed()
 
 if __name__ == "__main__":
     asyncio.run(main())
