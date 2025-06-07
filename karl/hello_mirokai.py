6c9eec4004b38fbb2da810d6eb730c7cbbb33077
Karl Johannes <karl.frjo@gmail.com>
2025-06-07 14:31:42 +0100

    first museum guide script v1

--- karl/hello_mirokai.py
+++ karl/hello_mirokai.py
@@ -4,15 +4,11 @@ from pymirokai.models.data_models import Coordinates
 
 robot_ip = "10.32.38.150"
 api_key  = "admin"
-from pymirokai.models.data_models import Coordinates
 
 async def main():  
     async with connect(api_key, robot_ip) as robot:
-        # Example of code using the connected robot object
-        # await robot.say("Hello world").completed()
         mission = robot.go_to_relative(Coordinates(x=1.0, y=2.0, theta=0.0))
-    # When the scope is finished, the robot object is disconnected
+        mission = robot.say("Hello, world!")
 
 if __name__ == "__main__":
-    # asyncio.run() will create the event loop, run main(), and close the loop
     asyncio.run(main())
